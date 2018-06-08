b175dcbfc20174504ace56b571b0619e56dc0ffd
Chen Dong <dchen15@dow.com>
2018-06-08 21:15:10 +0800

    init project with material ui

---
+++ QuestionnaireWeb/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
